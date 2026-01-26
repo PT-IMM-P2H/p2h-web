be4769d203cdbe3ba48bd3fd873516471a478112
Noval ian <andrianau26@gmail.com>
2026-01-26 11:50:11 +0800

    main js

--- frontend-p2h/src/main.js
+++ frontend-p2h/src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import router from './router/indext.js'
+import router from './router/index.js'
 import i18n from './lang/index.js'
 
 createApp(App).use(router).use(i18n).mount('#app')
