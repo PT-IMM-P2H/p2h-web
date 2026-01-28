3b73b91e75196d625afe615394bf082d71f3cf7b
Yui Yunnifa <43847873+Yunnifa@users.noreply.github.com>
2026-01-28 10:41:39 +0800

    Add extend_existing to Vehicle table arguments

--- backend/app/models/vehicle.py
+++ backend/app/models/vehicle.py
@@ -33,6 +33,7 @@ class UnitKategori(enum.Enum):
 
 class Vehicle(Base):
     __tablename__ = "vehicles"
+    __table_args__ = {'extend_existing': True}
     
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     no_lambung = Column(String(50), unique=True, nullable=True, index=True)
