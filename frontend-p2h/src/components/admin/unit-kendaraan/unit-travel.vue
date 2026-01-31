40c034caa0ca82903270befc658dbbc0e133f04d
Noval ian <andrianau26@gmail.com>
2026-01-31 23:02:20 +0800

    icon tambah kendaraan

--- frontend-p2h/src/components/admin/unit-kendaraan/unit-travel.vue
+++ frontend-p2h/src/components/admin/unit-kendaraan/unit-travel.vue
@@ -10,6 +10,7 @@ import {
   ArrowUpTrayIcon,
   ArrowDownTrayIcon,
   TrashIcon,
+  PlusIcon,
   UserPlusIcon,
   CloudArrowUpIcon,
   PencilSquareIcon,
@@ -496,7 +497,7 @@ const getDateStyle = (dateString) => {
                       @click="opentambahUnitKendaraan"
                       class="flex items-center gap-2 px-3 py-2 border border-gray-300 rounded-md text-white bg-[#6444C6] hover:bg-[#5c3db8] transition text-sm"
                     >
-                      <UserPlusIcon class="w-5 h-5" />
+                      <PlusIcon class="w-5 h-5" />
                       <span>Tambah unit kendaraan</span>
                     </button>
 
